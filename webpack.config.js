c868909e771a0e563ed24b10aa5a3b985692fec0
Mike Starr <mstarr@rsui.com>
2019-05-24 10:03:47 -0400

    webpack.config as function and extract css

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,67 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: {
-    main: "./app/index.tsx"
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[hash].js",
-    publicPath: ""
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"]
-  },
-  optimization: {
-    minimize: true,
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          chunks: "initial",
-          minChunks: 2
-        },
-        vendor: {
-          test: /node_modules/,
-          chunks: "initial",
-          name: "vendor",
-          priority: 10,
-          enforce: true
-        }
-      }
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1
-            }
+module.exports = env => {
+  console.log(env);
+  const isDevelop = env.mode === "development" ? true : false;
+  return {
+    entry: {
+      main: "./app/index.tsx"
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].[hash].js",
+      publicPath: ""
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    optimization: {
+      minimize: true,
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            chunks: "initial",
+            minChunks: 2
+          },
+          vendor: {
+            test: /node_modules/,
+            chunks: "initial",
+            name: "vendor",
+            priority: 10,
+            enforce: true
           }
-        ]
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.(ttf|eot|otf|svg|jpe?g|png|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader",
-        options: {
-          limit: 10000
         }
       }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isDevelop ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader"
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new HtmlWebpackPlugin({
+        title: "Simple React App",
+        template: "app/index.html"
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].[hash].css",
+        chunkFilename: "[id].[hash].css"
+      })
     ]
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({
-      title: "Simple React App",
-      template: "app/index.html"
-    })
-  ]
+  };
 };
