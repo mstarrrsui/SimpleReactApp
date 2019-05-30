c20a25cdddcff8f8e3eb6942353307652f884c57
Mike Starr <mstarr@rsui.com>
2019-05-30 17:11:35 -0400

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,56 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: {
-    main: "./app/index.tsx"
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[hash].js",
-    publicPath: ""
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"]
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all"
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+module.exports = ({ mode } = { mode: "development" }) => {
+  console.log(`mode: ${mode}`);
+
+  const isDevelop = mode === "development";
+
+  return {
+    entry: {
+      main: "./app/index.tsx"
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].[hash].js",
+      publicPath: ""
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all"
       }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isDevelop ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader"
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "[name].[hash].css",
+        chunkFilename: "[id].[hash].css"
+      }),
+      new HtmlWebpackPlugin({
+        title: "Simple React App",
+        template: "app/index.html"
+      })
     ]
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({
-      title: "Simple React App",
-      template: "app/index.html"
-    })
-  ]
+  };
 };
