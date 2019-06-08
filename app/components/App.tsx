ee345305404c3cbc6daac99254639de77f07f8f5
Mike Starr <mstarr@rsui.com>
2019-06-08 17:44:47 -0400

    transition

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
 
-import Boxes from "./Boxes";
-import Button from "devextreme-react/button";
 import SearchInput from "./SearchInput";
 
-import "devextreme/dist/css/dx.common.css";
-import "devextreme/dist/css/dx.light.css";
+//import "devextreme/dist/css/dx.common.css";
+//import "devextreme/dist/css/dx.light.css";
 
 export default class App extends React.Component {
   sayHelloWorld(): void {
@@ -17,11 +15,6 @@ export default class App extends React.Component {
       <div>
         <h2>Hello, from my simple React App</h2>
         <SearchInput />
-        <Button text="Click me" onClick={this.sayHelloWorld} />
-        <div className="mainimgcontainer">
-          <div className="mainimg" />
-        </div>
-        <Boxes />
       </div>
     );
   }
