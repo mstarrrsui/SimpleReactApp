05785adcb4f4db35d2790fd7b7cd8c80b7a78740
Starr <mstarr@rsui.com>
2019-06-19 13:32:15 -0400

    p-2

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:@typescript-eslint/eslint-recommended",
+    "prettier",
+    "prettier/@typescript-eslint"
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true
+    },
+    project: "./tsconfig.json"
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "linebreak-style": 0,
+    "lines-between-class-members": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/explicit-member-accessibility": "off"
+  }
+};
