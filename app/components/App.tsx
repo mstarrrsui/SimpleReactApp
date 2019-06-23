f3be36f1c99b2021dcce9ad864c4415fc58900a6
Mike Starr <mstarr@rsui.com>
2019-06-23 10:53:14 -0400

    step-1

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,16 +1,13 @@
 import * as React from "react";
 
-import Boxes from "./Boxes";
+import Tasks from "./Tasks";
 
 export default class App extends React.Component {
   public render(): React.ReactNode {
     return (
       <div>
-        <h2>Hello, from my simple React App</h2>
-        <div className="mainimgcontainer">
-          <div className="mainimg" />
-        </div>
-        <Boxes />
+        <h2>Tasks</h2>
+        <Tasks />
       </div>
     );
   }
---
+++ app/components/Tasks.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+
+const DATA_URL = "http://localhost:3130/tasks/5480";
+
+interface State {
+  tasks: any[];
+}
+
+const initialState: State = {
+  tasks: []
+};
+export default class App extends React.Component<object, State> {
+  public state: State = initialState;
+
+  public componentDidMount(): void {
+    fetch(DATA_URL)
+      .then(res => res.json())
+      .then(tasks => {
+        this.setState({ tasks });
+      });
+  }
+
+  public render(): React.ReactNode {
+    const { tasks } = this.state;
+
+    return (
+      <div>
+        {tasks.map(task => (
+          <div key={task.TaskID}>
+            {task.CurrentStackDescription}-{task.CurrentStepDescription} -{" "}
+            {task.InsuredName}
+          </div>
+        ))}
+        <div>Stack-Step - Status - Insured Name</div>
+      </div>
+    );
+  }
+}
