41cc13699dab9021fce07699e449415f13d50f89
Mike Starr <mstarr@rsui.com>
2019-06-23 11:27:30 -0400

    loading and error handling

--- app/components/Tasks.tsx
+++ app/components/Tasks.tsx
@@ -10,31 +10,54 @@ interface Task {
 }
 
 interface State {
+  isLoading: boolean;
+  error: Error | null;
   tasks: Task[];
 }
 
 const initialState: State = {
+  isLoading: false,
+  error: null,
   tasks: []
 };
 export default class App extends React.Component<object, State> {
   public state: State = initialState;
 
   public componentDidMount(): void {
+    this.setState({ isLoading: true, error: null });
     fetch(DATA_URL)
-      .then(res => res.json())
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          console.log("error...");
+          throw Error("Error fetching the data!!!");
+        }
+      })
       .then(tasks => {
-        this.setState({ tasks });
+        this.setState({ tasks, isLoading: false });
+      })
+      .catch(error => {
+        this.setState({ isLoading: false, error });
       });
   }
 
   public render(): React.ReactNode {
-    const { tasks } = this.state;
+    const { tasks, isLoading, error } = this.state;
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    if (isLoading) {
+      return <p>Please wait... loading......</p>;
+    }
 
     return (
       <div>
         {tasks.map(task => (
-          <div key={task.TaskID}>
-            {task.CurrentStackDescription}-{task.CurrentStepDescription} -{" "}
+          <div className="task" key={task.TaskID}>
+            {task.CurrentStackDescription}-{task.CurrentStepDescription} -
             {task.InsuredName}
           </div>
         ))}
