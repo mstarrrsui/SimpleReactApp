d8ccdbc008289fefe8bcce10505f6114424980fd
Mike Starr <mstarr@rsui.com>
2019-07-31 14:26:36 -0400

    reset to initial

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
 
-//import Tasks from "./Tasks";
-//import TasksRenderProp from "./TasksRenderProp";
-import TasksHooks from "./TasksHooks";
+import Tasks from "./Tasks";
 
 export default class App extends React.Component {
   public render(): React.ReactNode {
     return (
       <div>
         <h2>Tasks</h2>
-        <TasksHooks />
+        <Tasks />
       </div>
     );
   }
